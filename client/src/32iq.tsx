209a23c7c7d323a2a2b5530892350d55ce24d4df
jonathan-whiteley <76531592+jonathan-whiteley@users.noreply.github.com>
2023-04-15 20:05:36 -0700

    home page and header update

--- client/src/32iq.tsx
+++ client/src/32iq.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button} from "semantic-ui-react";
+// import {Button} from "semantic-ui-react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Search from "./components/Integrations/Search";
@@ -7,6 +7,26 @@ import Liveboard from "./components/Integrations/Liveboard";
 import {Menu} from "semantic-ui-react";
 import AnalyzeImage from "./components/AnalyzeImage/AnalyzeImage";
 import About from "./components/About/About";
+import Data from "./components/Data/Data";
+
+
+import {
+    Button,
+    Collapse,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    UncontrolledDropdown,
+    NavbarBrand,
+    Navbar,
+    NavItem,
+    NavLink,
+    Nav,
+    Container,
+    Row,
+    Col,
+    UncontrolledTooltip
+  } from "reactstrap";
 
 
 function displayGrid(menuComponent: JSX.Element, applicationComponent: JSX.Element) {
@@ -23,61 +43,167 @@ function displayGrid(menuComponent: JSX.Element, applicationComponent: JSX.Eleme
 }
 
 function getMenu(activeItem: string) {
+
     return (
-        <Menu className={"header-menu"}>
-            <div>
-               <Link to="/home">
-                    <Menu.Item
-                        className="title-button"
-                        name="home"
-                        active={activeItem === "home"}
-                    >
-                        <h1 className="title">32iQ • Panoramic X-Ray Assistant</h1>
-                    </Menu.Item>
-                </Link>
-            </div>
+<Navbar className={"fixed-top "} color-on-scroll="100" expand="lg">
+      <Container>
+        <div className="navbar-translate">
+          <NavbarBrand to="/home" tag={Link} id="navbar-brand">
+            <span>32iQ • </span>
+            Panoramic X-ray Assistant
+          </NavbarBrand>
+          <button
+            className="navbar-toggler navbar-toggler"
+          >
+            <span className="navbar-toggler-bar bar1" />
+            <span className="navbar-toggler-bar bar2" />
+            <span className="navbar-toggler-bar bar3" />
+          </button>
+        </div>
+        <Collapse
+          className={"justify-content-end "}
+          navbar
+        >
+          <div className="navbar-collapse-header">
+            <Row>
+              <Col className="collapse-brand" xs="6">
+                <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                  32iQ•
+                </a>
+              </Col>
+              <Col className="collapse-close text-right" xs="6">
+                <button
+                  className="navbar-toggler"
+                >
+                  <i className="tim-icons icon-simple-remove" />
+                </button>
+              </Col>
+            </Row>
+          </div>
+          <Nav navbar>
+            <NavItem>
+              <NavLink 
+              to="/" 
+              tag={Link}>
+              Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                to="/about-us"
+                tag={Link} >
+                About
+              </NavLink>
+           </NavItem>
+           <NavItem>
+              <NavLink
+                to="/data-page"
+                tag={Link} >
+                Data
+              </NavLink>
+           </NavItem>
+           
+            <UncontrolledDropdown nav>
+              <DropdownToggle
+                caret
+                color="default"
+                data-toggle="dropdown"
+                href="#pablo"
+                nav
+                onClick={(e) => e.preventDefault()}
+              >
+                <i className="fa fa-cogs d-lg-none d-xl-none" />
+                Demo
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-with-icons">
+                <DropdownItem href="/patient-dashboard">
+                <i className="tim-icons icon-image-02" />
+                  PatientiQ
+                </DropdownItem>
+                <DropdownItem href="/patient-search">
+                  <i className="tim-icons icon-zoom-split" />
+                  SearchiQ
+                </DropdownItem>
+                <DropdownItem 
+                href="https://www.youtube.com/"
+                target="_blank">
+                  <i className="tim-icons icon-video-66" />
+                  Video Demo
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem> 
+            <Button 
+            className="nav-link d-none d-lg-block"
+            color="primary"
+            href="/analyze-image">
+            <i className="tim-icons icon-cloud-upload-94" /> Analyze Image
+         </Button>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
 
-            <div className={"header-buttons"}>
-                <Link className="header-button" to="/analyze-image">
-                    <Menu.Item
-                        name="analyze-image"
-                        active={activeItem === "analyze-image"}
-                    >
-                        <Button
-                            className="header-button"
-                        >
-                            Analyze Image
-                        </Button>
-                    </Menu.Item>
-                </Link>
 
-                <Link className="header-button" to="/patient-dashboard">
-                    <Menu.Item
-                        name="patient-dashboard"
-                        active={activeItem === "patient-dashboard"}
-                    >
-                        <Button
-                            className="header-button"
-                        >
-                            Patient Dashboard
-                        </Button>
-                    </Menu.Item>
-                </Link>
+    //*** */
 
-                <Link className="header-button" to="/patient-search">
-                    <Menu.Item
-                        name="patient-search"
-                        active={activeItem === "patient-search"}
-                    >
-                        <Button
-                            className="header-button"
-                        >
-                            Patient Search
-                        </Button>
-                    </Menu.Item>
-                </Link>
-            </div>
-        </Menu>
+        // <Menu className={"header-menu"}>
+        //     <div>
+        //        <Link to="/home">
+        //             <Menu.Item
+        //                 className="title-button"
+        //                 name="home"
+        //                 active={activeItem === "home"}
+        //             >
+        //                 <h1 className="title">32iQ • Panoramic X-Ray Assistant</h1>
+        //             </Menu.Item>
+        //         </Link>
+        //     </div>
+
+        //     <div className={"header-buttons"}>
+        //         <Link className="header-button" to="/analyze-image">
+        //             <Menu.Item
+        //                 name="analyze-image"
+        //                 active={activeItem === "analyze-image"}
+        //             >
+        //                 <Button
+        //                     className="header-button"
+        //                 >
+        //                     Analyze Image
+        //                 </Button>
+        //             </Menu.Item>
+        //         </Link>
+
+        //         <Link className="header-button" to="/patient-dashboard">
+        //             <Menu.Item
+        //                 name="patient-dashboard"
+        //                 active={activeItem === "patient-dashboard"}
+        //             >
+        //                 <Button
+        //                     className="header-button"
+        //                 >
+        //                     Patient Dashboard
+        //                 </Button>
+        //             </Menu.Item>
+        //         </Link>
+
+        //         <Link className="header-button" to="/patient-search">
+        //             <Menu.Item
+        //                 name="patient-search"
+        //                 active={activeItem === "patient-search"}
+        //             >
+        //                 <Button
+        //                     className="header-button"
+        //                 >
+        //                     Patient Search
+        //                 </Button>
+        //             </Menu.Item>
+        //         </Link>
+        //     </div>
+        // </Menu>
+
+
     )
 }
 
@@ -114,6 +240,12 @@ const Application32IQ = () => {
                 <Route path="/about-us" children={() =>
                     displayGrid(
                         getMenu("about-us"),
+                        <About />,
+                    )} />
+
+                <Route path="/data-page" children={() =>
+                    displayGrid(
+                        getMenu("data-page"),
                         <About />,
                     )} />
             </Switch>
