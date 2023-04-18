32597a110b22dcf8b80c0ebc4a07a37c3e63d80b
ALEXA COUGHLIN <ALEXACOUGHLIN1@GMAIL.COM>
2023-04-17 22:14:15 -0700

    Updating bios and columns

--- src/components/profile-page.js
+++ src/components/profile-page.js
@@ -21,19 +21,19 @@ function About() {
                         
                   </div> 
            
-          <div class="col-sm-6 col-md-4">
+          <div class="col-sm-6 col-md-3">
 
                 <div class="team-item">
                 
                     <img src={Alexa} class="team-img" alt="pic" />                   
                     <h3>ALEXA COUGHLIN</h3>            
                     <div class="team-info"><p>Product Manager</p></div>
-                    <div class="bio"><p>Short bio.</p></div>
+                    <div class="bio"><p>Alexa brings a background in Human-Computer interaction design and data analytics experience to the team. She currently works as a Business Analyst at SpaceX and spends her free time hiking and surfing.</p></div>
                
               </div>
           </div> 
             
-          <div class="col-sm-6 col-md-4">
+          <div class="col-sm-6 col-md-3">
 
                 <div class="team-item">
                 
@@ -43,12 +43,12 @@ function About() {
                     
                     <div class="team-info"><p>ML Engineer</p></div>
 
-                    <div class="bio"><p>Short bio.</p></div>
+                    <div class="bio"><p>Connor is a Data Scientist at Predibase where he is excited to be working on an end to end declarative machine learning platform poised to revolutionize the way machine learning is done throughout the industry.</p></div>
                     
                 </div>
 
           </div> 
-          <div class="col-sm-6 col-md-4">
+          <div class="col-sm-6 col-md-3">
 
                 <div class="team-item">
                 
@@ -58,12 +58,12 @@ function About() {
                     
                     <div class="team-info"><p>Data Scientist/Project Manager</p></div>
 
-                    <div class="bio"><p>Short bio.</p></div>
+                    <div class="bio"><p>Jonathan is a Sr Solutions Engineer at ThoughtSpot, where he works to bring AI-powered Search + Analytics to enterprise data. Outside of the (virtual) office, he enjoys skiing, sport-climbing, and cooking.</p></div>
                     
                 </div>
 
           </div>
-          <div class="col-sm-6 col-md-4">
+          <div class="col-sm-6 col-md-3">
 
                 <div class="team-item">
                 
@@ -73,7 +73,7 @@ function About() {
                     
                     <div class="team-info"><p>BI/Data Engineer</p></div>
 
-                    <div class="bio"><p>Short bio.</p></div>
+                    <div class="bio"><p>Max is a Business Intelligence Associate at Citadel Securities who is passionate about Information Systems and their ability to make the world more fair and efficient through the lens of data analytics.</p></div>
                     
                 </div>
 
